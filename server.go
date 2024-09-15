30429080372c44861aebca661df12ec0d8e71692
Rishu Kumar <rishukumar.prince@gmail.com>
2024-09-15 13:53:57 +0530

    add db connection for MySQL and CRUD ops for Links

---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/imrishuroy/hackernews/graph"
+	database "github.com/imrishuroy/hackernews/internal/pkg/db/migrations/mysql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	database.InitDB()
+	defer func() {
+		err := database.CloseDB()
+		if err != nil {
+
+		}
+	}()
+	database.Migrate()
+
+	server := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
